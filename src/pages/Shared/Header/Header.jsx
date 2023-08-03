cf339cbb164277ed054edbb10dce06c1de11fdd3
Anika <tabassumanika210@gmail.com>
2023-08-04 05:54:08 +0600

    edit

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -7,8 +7,7 @@ import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../../Provider/AuthProvider";
 import { FaUserCircle } from "react-icons/fa";
-
-import Activelink from '../../Home/Home/Activelink/Activelink';
+import './../../Home/Home/Activelink';
 const Header = () => {
   const { user,logOut } = useContext(AuthContext);
   const handleLogOut=()=>{
@@ -19,28 +18,28 @@ const Header = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home" className="text-success">ChefVerse Bengal</Navbar.Brand>
+        <Navbar.Brand href="#home" className="text-success text-dark">ChefVerse Bengal</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto ">
             
-            <Activelink to="/" className="text-decoration-none  link-dark">Home</Activelink>
+              <Link to="/" className="text-decoration-none  link-dark">Home</Link>
             
              
-              <Activelink to="/blog" className="ms-3 text-decoration-none  link-dark" >Blog</Activelink>
+              <Link to="/blog" className="ms-3 text-decoration-none  link-dark" >Blog</Link>
            
           </Nav>
           {user?.email?<> <Nav.Link href="#profile">
-            <Activelink to="" className="text-decoration-none  link-dark me-2" >
-              <FaUserCircle style={{ fontSize: "2rem" }}>{user?.image}</FaUserCircle></Activelink>
+            <Link to="" className="text-decoration-none  link-dark me-2" >
+              <FaUserCircle style={{ fontSize: "2rem" }}>{user?.image}</FaUserCircle></Link>
               <span className='text-fuchsia-950 font-bold'>{user?.displayName}</span>
           </Nav.Link>
           
             <Button variant="success" className="d-flex justify-content-end text-decoration-none  " onClick={handleLogOut}>Log Out </Button> 
             </>:
-            <Activelink to="/login">
+            <Link to="/login">
               <Button variant="success" className="d-flex justify-content-end text-decoration-none  "  >Login </Button>
-            </Activelink>
+            </Link>
           }
 
         </Navbar.Collapse>
